52f902140545e104b0ef35e6980f0e0a0723f2c5
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 17:37:04 -0500

    00||First upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (i > 0) Console.Write("{0} ", array[i]);
+            else Console.WriteLine(array[i]);
+        }
+    }
+}
+
