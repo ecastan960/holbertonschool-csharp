c82cc4992d2b88708acd9f6abc5bae8cad7fc6d6
ecastan960 <ecastan1@eafit.edu.co>
2023-11-01 20:34:19 -0500

    00|First upload

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "";
+            for (int i = 97; i < 123; i++)
+            {
+                char character = (char)i;
+                string text = character.ToString();
+                alphabet = alphabet + text;
+            }
+            Console.Write("{0}", alphabet);
+        }
+    }
+}
