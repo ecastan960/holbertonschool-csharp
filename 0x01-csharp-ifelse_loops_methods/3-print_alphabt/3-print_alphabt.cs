10e9084a995dd1f71edd841682f7cc3975835587
ecastan960 <ecastan1@eafit.edu.co>
2023-11-01 20:56:55 -0500

    00|First upload

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "";
+            for (int i = 97; i < 123; i++)
+            {
+                char character = (char)i;
+                string text = character.ToString();
+                if (text == "q" || text == "e")
+                {
+                    continue;
+                }
+                alphabet = alphabet + text;
+            }
+            Console.Write("{0}", alphabet);
+        }
+    }
+}
