4250c5d25cfad80a56ccd72e5c7b24510f97cc8f
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 10:14:21 -0500

    00|first load

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _7_islower
+{
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+            string s1 = CharUnicodeInfo.GetUnicodeCategory(c).ToString();
+            if (s1 == "LowercaseLetter")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
