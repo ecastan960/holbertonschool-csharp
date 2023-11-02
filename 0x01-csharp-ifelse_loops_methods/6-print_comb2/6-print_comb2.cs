b9f878c283f2299c750d9e2df34b9806c9b5ef04
ecastan960 <ecastan1@eafit.edu.co>
2023-11-01 22:00:45 -0500

    00|first upload

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    Console.Write("{0}{1}, ", i, j);
+                }
+            }
+            Console.Write("89\n");
+        }
+    }
+}
