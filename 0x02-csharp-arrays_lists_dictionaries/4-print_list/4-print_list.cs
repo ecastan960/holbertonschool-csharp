e0d94a86bf56419a4cf568197ce44fc289864c53
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 18:09:14 -0500

    00||first upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> iList = new List<int>();
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return iList;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            if (i < size - 1) Console.Write("{0} ", i);
+            else Console.WriteLine(i);
+            iList.Add(i);
+        }
+        return iList;
+    }
+}
