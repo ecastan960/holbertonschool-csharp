4e903a01ebd55f79a2ea66340d10e6fac5a78d53
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 12:38:08 -0500

    00|first upload

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0) Console.Write("Fizz");
+                else if (i % 5 == 0 && i % 3 != 0) Console.Write("Buzz");
+                else if (i % 3 == 0 && i % 5 == 0) Console.Write("FizzBuzz");
+                else Console.Write(i);
+                if (i < 100) Console.Write(" ");
+            }
+        }
+    }
+}
