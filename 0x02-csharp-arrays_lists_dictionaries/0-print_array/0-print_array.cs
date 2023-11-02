2672a446f1137c20f7462c92a4c5812ab076dd73
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 15:21:01 -0500

    02|Fixed output

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -6,7 +6,16 @@ namespace _0_print_array
     {
         public static int[] CreatePrint(int size)
         {
-            if (size < 0) return null;
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            if (size == 0)
+            {
+                Console.WriteLine();
+                return new int[0];
+            }
             int[] array = new int[size];
 
             for (int i = 0; i < size; i++)
