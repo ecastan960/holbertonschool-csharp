c86df6ca7bf6e38528ac257ced3f2b6ef0693130
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 10:45:17 -0500

    00|first upload

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int lastNumber = number % 10;
+            if (lastNumber < 0) lastNumber = lastNumber * -1;
+            Console.Write(lastNumber);
+            return lastNumber;
+        }
+    }
+}
