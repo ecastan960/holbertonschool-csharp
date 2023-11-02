111a0e72e2171c0b6f594c6873422541a34541e5
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 15:59:41 -0500

    04|remove namespace

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,30 +1,28 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
+    public static int[] CreatePrint(int size)
     {
-        public static int[] CreatePrint(int size)
+        if (size < 0)
         {
-            if (size < 0)
-            {
-                Console.Write("Size cannot be negative");
-                return null;
-            }
-            if (size == 0)
-            {
-                Console.WriteLine();
-                return new int[0];
-            }
-            int[] array = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = i;
-                if (i < size - 1) Console.Write("{0} ", i);
-                else Console.WriteLine(i);
-            }
-            return array;
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            if (i < size - 1) Console.Write("{0} ", i);
+            else Console.WriteLine(i);
+        }
+        return array;
     }
 }
+
