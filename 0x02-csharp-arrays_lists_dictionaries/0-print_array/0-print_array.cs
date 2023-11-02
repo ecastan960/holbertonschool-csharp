8595eec948e3e551f84307b4679c45f123bf4d48
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 15:12:50 -0500

    00|first upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0) return null;
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+                if (i < size - 1) Console.Write("{0} ", i);
+                else Console.WriteLine(i);
+            }
+            return array;
+        }
+    }
+}
