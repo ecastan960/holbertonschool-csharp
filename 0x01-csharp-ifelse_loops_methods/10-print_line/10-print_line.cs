cff8e4118968f19880444e713c79b8106cae2625
ecastan960 <ecastan1@eafit.edu.co>
2023-11-02 11:39:03 -0500

    00|first load

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine();
+        }
+    }
+}
