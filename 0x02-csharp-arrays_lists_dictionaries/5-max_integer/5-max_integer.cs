be3d4235c39ce92e45e8fe0a12c517923a4e55e6
ecastan960 <ecastan1@eafit.edu.co>
2023-11-08 13:33:46 -0500

    00|First upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int max = 0;
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] > max) max = myList[i];
+        }
+        return max;
+    }
+}
+
