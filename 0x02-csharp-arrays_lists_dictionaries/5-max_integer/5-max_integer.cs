e457bb939b812d8c5b497c7c5a7853c4a4a64835
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-08 13:52:05 -0500

    01| Add edge case empty list

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,6 +5,11 @@ class List
 {
     public static int MaxInteger(List<int> myList)
     {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
         int max = 0;
         for (int i = 0; i < myList.Count; i++)
         {
