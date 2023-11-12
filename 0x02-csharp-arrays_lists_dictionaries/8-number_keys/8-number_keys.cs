e5a3ea393f55f49fc410e238057753c82c55de70
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-12 00:01:01 -0500

    00|First upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int numberKeys = 0;
+        foreach (KeyValuePair<string, string> ele in myDict)
+        {
+            numberKeys += 1;
+        }
+        return numberKeys;
+    }
+}
+
