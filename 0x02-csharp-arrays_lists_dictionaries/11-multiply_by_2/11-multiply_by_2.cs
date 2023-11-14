e8fc6538b77b6b161a4c0721bc4288d379345f23
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-13 21:05:16 -0500

    00| first upload test 11

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
+        foreach (KeyValuePair<string, int> el in myDict)
+        {
+            newDict[el.Key] = el.Value * 2;
+        }
+        return newDict;
+    }
+}
+
