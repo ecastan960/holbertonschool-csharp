e59c797c9274716937aed2054aa61520c2cf48a5
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-14 14:02:12 -0500

    00|First upload task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        foreach (int el in list1)
+        {
+            if (list1.Contains(el) && list2.Contains(el))
+            {
+                newList.Add(el);
+            }
+        }
+        return newList;
+    }
+}
