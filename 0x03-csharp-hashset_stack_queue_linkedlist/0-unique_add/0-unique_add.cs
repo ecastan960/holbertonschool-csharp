79312bb5bf86299774b3523d8c64bc454fca1677
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-14 12:40:20 -0500

    00|First upload task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> myHash = new HashSet<int>();
+        int sum = 0;
+        foreach (int n in myList)
+        {
+            myHash.Add(n);
+        }
+        foreach (int s in myHash)
+        {
+            sum = sum + s;
+        }
+        return sum;
+    }
+}
+
