70d4802580404b0869775e7d255924e7be193894
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-14 00:03:10 -0500

    00| first upload task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] intarray = new int[5, 5];
+            intarray[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j < 4)
+                    {
+                        Console.Write("{0} ", intarray[i, j]);
+                    }
+                    else
+                    {
+                        Console.WriteLine(intarray[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
