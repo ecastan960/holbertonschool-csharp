be2fb0043997a7dfe7ebbadac654f02028c1fc78
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-13 22:49:52 -0500

    00|FIrst upload task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0) return "None";
+        var sortedDict = myList.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+        var last = sortedDict.Last();
+        return last.Key;
+    }
+}
+
