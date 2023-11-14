38f26f96cefc5571df6f9c403cc39f2c22053457
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-13 22:09:55 -0500

    00|first upload task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sortedDict = myDict.OrderBy(pair => pair.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
+        foreach (KeyValuePair<string, string> pair in sortedDict)
+        {
+            Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+        }
+    }
+}
