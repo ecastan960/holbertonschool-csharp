5d2aa243be666569d78c769d43f807b6948f4333
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-14 14:51:00 -0500

    00|First upload task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        foreach (int el in list1)
+        {
+            if (!(list1.Contains(el) && list2.Contains(el)))
+            {
+                newList.Add(el);
+            }
+        }
+        foreach (int el in list2)
+        {
+            if (!(list2.Contains(el) && list1.Contains(el)))
+            {
+                newList.Add(el);
+            }
+        }
+        newList.Sort();
+        return newList;
+    }
+}
+
