99056589d3021c0d2e269307b43cd07ee81e480e
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-16 10:27:07 -0500

    00| first upload task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Stack<string> copyStack = new Stack<string>();
+        foreach (string item in aStack)
+        {
+            copyStack.Push(item);
+        }
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+        Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+        if (aStack.Contains(search) == true)
+        {
+            foreach (string item in copyStack)
+            {
+                aStack.Pop();
+                if (item == search)
+                {
+                    aStack.Pop();
+                    break;
+                }
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
