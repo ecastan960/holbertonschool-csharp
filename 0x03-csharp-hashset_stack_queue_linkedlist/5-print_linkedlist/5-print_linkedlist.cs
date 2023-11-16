fae765d92546a5dd962e60473aa2d7c737771bcc
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-16 12:51:14 -0500

    00|First upload task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> my_list = new LinkedList<int>();
+        if (size < 0) return my_list;
+        for (int i = 0; i < size; i++)
+        {
+            my_list.AddLast(i);
+        }
+        foreach (int j in my_list)
+        {
+            Console.WriteLine(j);
+        }
+        return my_list;
+    }
+}
+
+
