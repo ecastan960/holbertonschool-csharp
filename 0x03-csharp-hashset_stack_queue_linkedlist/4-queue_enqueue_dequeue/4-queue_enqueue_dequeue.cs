b8976db70828308d99e251d61cd357e922ebc152
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-16 12:34:45 -0500

    00|first upload task 03

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Queue<string> copyQueue = new Queue<string>();
+        foreach (string item in aQueue)
+        {
+            copyQueue.Enqueue(item);
+        }
+
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        if (aQueue.Contains(search) == true)
+        {
+            foreach (string item in copyQueue)
+            {
+                aQueue.Dequeue();
+                if (item == search)
+                {
+                    break;
+                }
+            }
+        }
+        return aQueue;
+    }
+}
+
