010341f879d30671898bfb9565b10564851485d2
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-19 02:38:22 -0500

    00|First upload task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (int node in myLList)
+        {
+            if (index == n) return node;
+            index += 1;
+        }
+        return 0;
+    }
+}
+
