44e0bd27bea7c0b3f2a9a59fd8dd3902597b8ee0
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-19 00:28:12 -0500

    00| first upload

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int node = 0;
+        int count = 0;
+        foreach (int num in myLList)
+        {
+            if (count == index)
+            {
+                node = num;
+                break;
+            }
+            count += 1;
+
+        }
+        myLList.Remove(node);
+    }
+}
+
