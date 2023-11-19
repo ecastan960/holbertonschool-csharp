97a35d49ead31427a944946997f911382c08709c
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-19 03:22:03 -0500

    00|First upload task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        int nextNodeValue = 0;
+        foreach (int number in myLList)
+        {
+            if (number > n)
+            {
+                nextNodeValue = number;
+                break;
+            }
+        }
+        LinkedListNode<int> temp = myLList.Find(nextNodeValue);
+        myLList.AddBefore(temp, n);
+        return myLList.Find(n);
+    }
+}
+
