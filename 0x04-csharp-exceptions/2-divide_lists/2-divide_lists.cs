7c09aa814f432a52ce77271cdb35a4670c31fa2c
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-21 14:17:58 -0500

    00 | FIrst upload task 2

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        int index;
+
+        for (index = 0; index < listLength; index++)
+        {
+            try
+            {
+                result.Add(list1[index] / list2[index]);
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
+
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return result;
+    }
+}
