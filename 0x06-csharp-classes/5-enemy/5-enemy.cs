df2cf6bbd544f8e677457da925910e6b203cb060
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-25 17:09:23 -0500

    00 | first upload task 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> public class Zombie that defines a zombie.</summary>
+    class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Zombie Name: {Name} / Total Health: {GetHealth()}";
+        }
+    }
+}
