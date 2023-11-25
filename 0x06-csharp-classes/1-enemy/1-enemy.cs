344e31755856af4d7c11198dc7fd07149ef9eca8
Esteban Castaño <ecastan1@eafit.edu.co>
2023-11-25 15:00:35 -0500

    00 | First upload task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> public class Zombie that defines a zombie.</summary>
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
