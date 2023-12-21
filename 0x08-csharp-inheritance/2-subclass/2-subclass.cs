0380facc57147aabfb8c33dbdfe7b33ba6b873ad
Esteban Castaño <ecastan1@eafit.edu.co>
2023-12-21 11:26:06 -0500

    00 | first upload task 0

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType) && baseType != derivedType) return true;
+        return false;
+    }
+}
+
