a14f9f710a3a1ba1e1ad4c5244197bb396412f73
Esteban Castaño <ecastan1@eafit.edu.co>
2023-12-21 01:01:42 -0500

    Update 0-is.cs

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// Class Obj add on
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// return true if obj is of type int
+    /// </summary>
+    /// <param name="obj">Object to compare</param>
+    /// <returns></returns>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj.GetType() == typeof(int)) return true;
