a2b4d3753266615239dcbafb6bab976e31c0ed9a
Esteban Castaño <ecastan1@eafit.edu.co>
2023-12-21 02:10:20 -0500

    00 | First upload of task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Class Obj add on
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// method that returns True if the object is an instance of, 
+    /// or if the object is an instance of a class that inherited from, Array, 
+    /// otherwise return False.
+    /// </summary>
+    /// <param name="obj">obj to compare</param>
+    /// <returns></returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType().IsSubclassOf(typeof(Array))) return true;
+        if (typeof(Array).IsInstanceOfType(obj.GetType())) return true;
+
+        return false;
+    }
+}
+
